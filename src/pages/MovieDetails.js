6d155a05a7f3fb861df53734a2e448cda617781f
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-10-17 01:01:38 +0530

    Updated movie details page

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const MovieDetails = () => {
@@ -19,24 +19,77 @@ const MovieDetails = () => {
         console.error("Error fetching movie details: ", error);
       }
     };
-
     fetchMovieDetails();
   }, [id]);
 
+  const isValidData = (data) => data && data !== "N/A";
+
   if (!movie) {
-    return <div>Loading...</div>;
+    return <div className='text-center'>Loading...</div>;
   }
 
   return (
-    <div className="flex flex-col md:flex-row ">
-      <img src={movie.Poster} alt={`${movie.Title} Poster`} className="mx-4 mb-4" />
-      <div className='flex flex-col w-2/3'>
-        <h2 className="text-3xl font-bold mb-4">{movie.Title} ({movie.Year})</h2>
-        <p><strong>Plot:</strong> {movie.Plot}</p>
-        <p><strong>Director:</strong> {movie.Director}</p>
-        <p><strong>Actors:</strong> {movie.Actors}</p>
-        <p><strong>Genre:</strong> {movie.Genre}</p>
-        <p><strong>IMDB Rating:</strong> {movie.imdbRating}</p>
+    <div className="flex flex-col ">
+      <div className='flex justify-between mb-6'>
+        <div>
+          <h2 className="text-5xl font-medium mb-4">{movie.Title}</h2>
+          <div className='flex gap-3 text-gray-300'>
+            {isValidData(movie.Year) && <p>{movie.Year}</p>}
+            {isValidData(movie.Rated) && <p>{movie.Rated}</p>}
+            {isValidData(movie.Runtime) && <p>{movie.Runtime}</p>}
+          </div>
+        </div>
+
+        <Link to={`https://www.imdb.com/title/${movie.imdbID}`} className="hidden md:block">
+          {isValidData(movie.imdbRating) &&
+            <div className='flex flex-col text-gray-300 items-start'>
+              <p className='text-sm font-medium'>IMDb RATING</p>
+              <p className='text-xl'><span className='text-white font-medium text-2xl'>{movie.imdbRating}</span>/10</p>
+              <p className='text-sm'>{movie.imdbVotes}</p>
+            </div>
+          }
+        </Link>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-6">
+        {isValidData(movie.Poster) && (
+          <img src={movie.Poster} alt={`${movie.Title} Poster`} className="" />
+        )}
+
+        <div className='flex flex-col w-full md:w-2/3 text-gray-300'>
+          <div className='flex flex-wrap gap-2 mt-2 mb-4'>
+            {isValidData(movie.Genre) && movie.Genre.split(', ').map((genre, index) => (
+              <span key={index} className="border border-gray-400 text-white px-3 py-1 rounded-full text-sm">
+                {genre}
+              </span>
+            ))}
+          </div>
+
+          {isValidData(movie.Plot) && <p className='mb-4 text-white'> {movie.Plot}</p>}
+
+          <Link to={`https://www.imdb.com/title/${movie.imdbID}`} className="block md:hidden">
+            {isValidData(movie.imdbRating) &&
+              <div className='flex text-gray-300 items-center gap-2 mb-4'>
+                <p className='text-sm font-bold'>IMDb RATING</p>
+                <p className='text-sm'><span className='text-white font-medium text-lg'>{movie.imdbRating}</span>/10</p>
+                <p className='text-sm'>{movie.imdbVotes}</p>
+              </div>
+            }
+          </Link>
+
+          {isValidData(movie.Director) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Director</span> {movie.Director}</p>}
+          {isValidData(movie.Writer) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Writers</span> {movie.Writer}</p>}
+          {isValidData(movie.Actors) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Stars</span> {movie.Actors}</p>}
+          {isValidData(movie.Language) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Language</span> {movie.Language}</p>}
+          {isValidData(movie.totalSeasons) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Seasons</span> {movie.totalSeasons}</p>}
+          {isValidData(movie.Released) && <p className='mb-3 pt-3 border-t-2 border-gray-800'>
+            <span className='text-white mb-4 mr-1 font-bold'>Released</span> {movie.Released}</p>}
+        </div>
       </div>
     </div>
   );
